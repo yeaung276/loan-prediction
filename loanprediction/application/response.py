d18838976543496e06cb27414aa73e574b542225
yeaung276 <yeyintaung.ya276@gmail.com>
2022-05-28 10:34:50 +0630

    add MediaResponse to return png files

--- loanprediction/application/response.py
+++ loanprediction/application/response.py
@@ -1,9 +1,18 @@
-from starlette.responses import JSONResponse as BaseResponse, StreamingResponse
+from io import BytesIO
+from typing import Any
+from starlette.responses import JSONResponse as BaseResponse
+from fastapi import status
+from fastapi.responses import Response as FResponse
 
 
 class Response(BaseResponse):
     pass
 
 
-class MediaResponse(StreamingResponse):
-    pass
+class MediaResponse(FResponse):
+    def __init__(self, content: BytesIO, media_type: str = "image/png") -> None:
+        super().__init__(
+            status_code=status.HTTP_200_OK,
+            content=content.read(),
+            media_type=media_type,
+        )
--- loanprediction/core/Entity/graph.py
+++ loanprediction/core/Entity/graph.py
@@ -8,4 +8,5 @@ class Graph:
         figure.savefig(self._data, format="png")
 
     def export(self) -> BytesIO:
+        self._data.seek(0)
         return self._data
