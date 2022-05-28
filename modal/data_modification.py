e69cbb34468a103345385057284e251a5404fb7f
yeaung276 <yeyintaung.ya276@gmail.com>
2022-05-28 13:30:58 +0630

    fix mypy error

--- modal/data_modification.py
+++ modal/data_modification.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pandas as pd
 
 # configs
@@ -5,8 +6,12 @@ TRAINING_DATA_PATH = "data/train_.csv"
 TESTING_DATA_PATH = "data/test_.csv"
 EMPTY_DATA_FILL_MODE = {"text": "mode", "number": "mean"}
 
-DROP_COLUMNS = [""]
+DROP_COLUMNS = ["Loan_ID"]
+
 
 def drop_columns(columns):
     csv_train = pd.read_csv(TRAINING_DATA_PATH)
-    print(csv_train.head)
+
+
+
+drop_columns(DROP_COLUMNS)
