87f7f40b0c325193e825d40578a23e792de54977
yeaung276 <yeyintaung.ya276@gmail.com>
2022-05-28 16:51:25 +0630

    add data_modification.py

--- modal/data_modification.py
+++ modal/data_modification.py
@@ -4,7 +4,8 @@ from pyparsing import col
 # configs
 TRAINING_DATA_PATH = "data/train_.csv"
 TESTING_DATA_PATH = "data/test_.csv"
-SAVE_FILE_PATH = "modal/intermediary_files"
+INTERMEDIATE_FILE_PATH = "modal/intermediary_files"
+FINAL_FILE_PATH = "modal"
 
 DROP_COLUMNS = ["Loan_ID"]
 MEANS_COLUMNS = ["LoanAmount", "ApplicantIncome", "CoapplicantIncome"]
@@ -28,7 +29,7 @@ CATEGORICAL_COLUMNS = [
 
 def drop_columns(dataframe, columns):
     dataframe.drop(columns=columns, inplace=True)
-    dataframe.to_csv(f"{SAVE_FILE_PATH}/csv_drop_unnecessary_col.csv")
+    dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_drop_unnecessary_col.csv")
 
 
 def fill_null_values_columns(dataframe, mean_columns, mode_columns):
@@ -37,21 +38,24 @@ def fill_null_values_columns(dataframe, mean_columns, mode_columns):
     for column in mode_columns:
         dataframe[column].fillna(dataframe[column].dropna().mode()[0], inplace=True)
     dataframe["Dependents"].replace("3+", 3, inplace=True)
-    dataframe.to_csv(f"{SAVE_FILE_PATH}/csv_filled_null.csv")
+    dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_filled_null.csv")
 
 
 def encode_categorical(dataframe, categorical_columns):
     dataframe = pd.get_dummies(dataframe, columns=categorical_columns)
-    dataframe.to_csv(f"{SAVE_FILE_PATH}/csv_one_hot_encoded.csv")
+    dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_one_hot_encoded.csv")
     return dataframe
 
-
-csv_train = pd.read_csv(TRAINING_DATA_PATH)
 # step: 1
-drop_columns(csv_train, DROP_COLUMNS)
+csv_train = pd.read_csv(TRAINING_DATA_PATH)
 # step: 2
-fill_null_values_columns(csv_train, MEANS_COLUMNS, MODE_COLUMNS)
+drop_columns(csv_train, DROP_COLUMNS)
 # step: 3
+fill_null_values_columns(csv_train, MEANS_COLUMNS, MODE_COLUMNS)
+# step: 4
 csv_train = encode_categorical(csv_train, CATEGORICAL_COLUMNS)
 
+csv_train.to_csv(f"{FINAL_FILE_PATH}/train_.csv")
+
+print(csv_train.head())
 print(csv_train.isnull().sum())
