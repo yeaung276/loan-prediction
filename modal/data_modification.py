380b7fe8674185755322c3936e6367e684c48e8c
yeaung276 <yeyintaung.ya276@gmail.com>
2022-05-28 23:09:14 +0630

    train with linear regression

--- modal/data_modification.py
+++ modal/data_modification.py
@@ -1,11 +1,9 @@
 import pandas as pd
-from pyparsing import col
 
 # configs
-TRAINING_DATA_PATH = "data/train_.csv"
-TESTING_DATA_PATH = "data/test_.csv"
+RAW_DATA_PATH = "data"
 INTERMEDIATE_FILE_PATH = "modal/intermediary_files"
-FINAL_FILE_PATH = "modal"
+FINAL_FILE_PATH = "modal/datas"
 
 DROP_COLUMNS = ["Loan_ID"]
 MEANS_COLUMNS = ["LoanAmount", "ApplicantIncome", "CoapplicantIncome"]
@@ -25,6 +23,14 @@ CATEGORICAL_COLUMNS = [
     "Self_Employed",
     "Property_Area",
 ]
+LABEL_COLUMN = "Loan_Status"
+NORMALIZE_COLUMN = [
+    "LoanAmount",
+    "ApplicantIncome",
+    "CoapplicantIncome",
+    "Dependents",
+    "Loan_Amount_Term",
+]
 
 
 def drop_columns(dataframe, columns):
@@ -38,6 +44,7 @@ def fill_null_values_columns(dataframe, mean_columns, mode_columns):
     for column in mode_columns:
         dataframe[column].fillna(dataframe[column].dropna().mode()[0], inplace=True)
     dataframe["Dependents"].replace("3+", 3, inplace=True)
+    dataframe["Dependents"] = pd.to_numeric(dataframe["Dependents"])
     dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_filled_null.csv")
 
 
@@ -46,14 +53,40 @@ def encode_categorical(dataframe, categorical_columns):
     dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_one_hot_encoded.csv")
     return dataframe
 
+
+def encode_label(dataframe, column):
+    dataframe[column].replace("Y", 1, inplace=True)
+    dataframe[column].replace("N", 0, inplace=True)
+    dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_label_encoded.csv")
+
+
+def data_nomalization_min_max(dataframe, columns):
+    for column in columns:
+        d_min = dataframe[column].min()
+        d_max = dataframe[column].max()
+        dataframe[columns] = (dataframe[columns] - d_min) / (d_max - d_min)
+    dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_normalized.csv")
+
+
+def data_nomalization_div_by_mean(dataframe, columns):
+    for column in columns:
+        mean = dataframe[column].mean()
+        dataframe[columns] = dataframe[columns] / mean
+    dataframe.to_csv(f"{INTERMEDIATE_FILE_PATH}/csv_normalized.csv")
+
+
 # step: 1
-csv_train = pd.read_csv(TRAINING_DATA_PATH)
+csv_train = pd.read_csv(f"{RAW_DATA_PATH}/train_.csv")
 # step: 2
 drop_columns(csv_train, DROP_COLUMNS)
 # step: 3
 fill_null_values_columns(csv_train, MEANS_COLUMNS, MODE_COLUMNS)
 # step: 4
 csv_train = encode_categorical(csv_train, CATEGORICAL_COLUMNS)
+# step: 5
+encode_label(csv_train, LABEL_COLUMN)
+# step: 6
+data_nomalization_div_by_mean(csv_train, NORMALIZE_COLUMN)
 
 csv_train.to_csv(f"{FINAL_FILE_PATH}/train_.csv")
 
---
+++ modal/training.py
@@ -0,0 +1,61 @@
+import os
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score
+from sklearn.linear_model import LogisticRegression
+
+MODAL_NAME = "logistic_regression_with_mean_normalization"
+MODAL_DESCRIPTION = "trained with div mean normalization on LoanAmount, ApplicantIncome, CoapplicantIncome, Dependent, Loan_Amount_Term"
+TRAINING_DATA_PATH = "modal/datas/train_.csv"
+Y_LABEL = "Loan_Status"
+OUTPUT_PATH = "modal/outputs"
+
+data = pd.read_csv(TRAINING_DATA_PATH)
+
+D_X = data.drop(Y_LABEL, axis=1)
+D_Y = data[Y_LABEL]
+
+X, X_test, Y, Y_test = train_test_split(D_X, D_Y, test_size=0.2)
+
+# logistic regression
+estimator = LogisticRegression()
+estimator.fit(X, Y)
+Y_p = estimator.predict(X_test)
+accuracy = accuracy_score(Y_test, Y_p)
+Y_pp = estimator.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = roc_curve(Y_test, Y_pp)
+auc = roc_auc_score(Y_test, Y_pp)
+
+# logging
+info = {
+    "name": MODAL_NAME,
+    "description": MODAL_DESCRIPTION,
+    "column_modification": "NONE",
+    "columns": [col for col in X],
+    "label": Y_LABEL,
+    "accuracy": accuracy,
+    "roc_accu": auc,
+}
+if not os.path.exists(f"{OUTPUT_PATH}/{MODAL_NAME}"):
+    os.mkdir(f"{OUTPUT_PATH}/{MODAL_NAME}")
+
+with open(f"{OUTPUT_PATH}/{MODAL_NAME}/{MODAL_NAME}.log", "w+") as log:
+    log.write("Modal Summary\n")
+    log.write("==============\n")
+    for key, value in info.items():
+        log.write(f"{key}: {value}")
+        log.write("\n")
+    log.write("End")
+
+plt.plot(fpr, tpr, label="validation, auc=" + str(auc))
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.legend(loc=4)
+plt.savefig(f"{OUTPUT_PATH}/{MODAL_NAME}/{MODAL_NAME}.png")
+
+with open(f"{OUTPUT_PATH}/{MODAL_NAME}/{MODAL_NAME}.pskl", "wb") as modal_file:
+    pickle.dump(estimator, modal_file)
+
+data.to_csv(f"{OUTPUT_PATH}/{MODAL_NAME}/{MODAL_NAME}.csv")
