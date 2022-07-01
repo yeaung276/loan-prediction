1a57263dcc2f259c0ece7bebbea46321b437093c
yeaung276 <yeyintaung.ya276@gmail.com>
2022-07-01 23:11:56 +0700

    add export script

--- scripts/export.py
+++ scripts/export.py
@@ -7,13 +7,16 @@ BASE_MODAL_PATH = "ModalTraining/output/"
 
 
 def export(args: list[str] = sys.argv) -> None:
-    modal_name = args[0]
+    modal_name = args[1]
     src_dir = os.path.join(BASE_MODAL_PATH, modal_name)
     dst_dir = os.path.join(EXPORT_PATH)
-    shutil.copy(
-        os.path.join(src_dir, f"{modal_name}.pskl"), os.path.join(dst_dir, "modal.pskl")
-    )
-    shutil.copy(
-        os.path.join(src_dir, f"{modal_name}.log"),
-        os.path.join(dst_dir, "description.txt"),
-    )
+    try:
+        shutil.copy(
+            os.path.join(src_dir, f"{modal_name}.pskl"), os.path.join(dst_dir, "modal.pskl")
+        )
+        shutil.copy(
+            os.path.join(src_dir, f"{modal_name}.log"),
+            os.path.join(dst_dir, "description.txt"),
+        )
+    except FileNotFoundError as err:
+        raise Exception('Modal with given name does not exist!') from err
